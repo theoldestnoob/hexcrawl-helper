7e2f1c0b7a24eeb6f67ed1075096f9f1b5f886dd
theoldestnoob <will.hinz@gmail.com>
2019-08-22 02:41:59 -0500

    updated DieExpr to use negative DieSets; DieExpr.probabilities not working yet

--- dice.py
+++ dice.py
@@ -7,7 +7,7 @@ Created on Wed Aug 21 19:29:51 2019
 
 import re
 from random import randint
-from itertools import product
+from itertools import product, chain
 
 
 class DieSet:
@@ -83,7 +83,7 @@ class DieSet:
         for i in range(self.minroll, self.maxroll + 1):
             numerator_dict[i] = 0
         if self.negative:
-            allrolls = product(range(self.sides * -1, 0, 1), repeat=self.num)
+            allrolls = product(range(self.sides * -1, 0), repeat=self.num)
         else:
             allrolls = product(range(1, self.sides + 1), repeat=self.num)
         for roll in allrolls:
@@ -97,57 +97,102 @@ class DieSet:
 class DieExpr:
     def __init__(self, dice_string):
         self.dstring = dice_string
-        self.addlist = []
-        self.sublist = []
-        d_tokens = dstring_parse(dice_string)
-        add = False
-        sub = False
-        for token in d_tokens:
-            if token == "+":
-                add = True
-            elif token == "-":
-                sub = True
-            else:
-                if not add and not sub:
-                    self.addlist.append(token)
-                elif add:
-                    add = False
-                    self.addlist.append(token)
-                elif sub:
-                    sub = False
-                    self.sublist.append(token)
+        self.diesets = dstring_parse(dice_string)
+        self.prob_list = None
 
     def __repr__(self):
         return f"DieExpr('{self.dstring}')"
 
     def roll(self):
         total = 0
-        for dieset in self.addlist:
+        for dieset in self.diesets:
             total += dieset.roll()
-        for dieset in self.sublist:
-            total -= dieset.roll()
         return total
 
     @property
     def minroll(self):
         total = 0
-        for dieset in self.addlist:
+        for dieset in self.diesets:
             total += dieset.minroll
-        for dieset in self.sublist:
-            total -= dieset.maxroll
         return total
 
     @property
     def maxroll(self):
         total = 0
-        for dieset in self.addlist:
+        for dieset in self.diesets:
             total += dieset.maxroll
-        for dieset in self.sublist:
-            total -= dieset.minroll
         return total
 
+    @property
+    def probabilities(self):
+        if self.prob_list is None:
+            self.prob_list = self._probabilities()
+        return self.prob_list
+
+    def probability(self, num):
+        if not self.minroll <= num <= self.maxroll:
+            return 0
+        else:
+            for roll, prob in self.probabilities:
+                if roll == num:
+                    return prob
+            return 0
+
+    # TODO: fix this it ain't work so good
+    '''
+    possibly helpful? behavior of itertools.product()
+    a = [1, 2, 3]
+    b = [3, 4, 5]
+    c = [a, b]
+
+    for i in product(c):
+        print(i)
+
+    ([1, 2, 3],)
+    ([3, 4, 5],)
+
+    for i in product(*c):
+        print(i)
+
+    (1, 3)
+    (1, 4)
+    (1, 5)
+    (2, 3)
+    (2, 4)
+    (2, 5)
+    (3, 3)
+    (3, 4)
+    (3, 5)
+    '''
     def _probabilities(self):
-        pass
+        results = []
+        setrolls = []
+        allrolls = []
+        denominator = 0
+        for dieset in self.diesets:
+            denominator += dieset.sides ** dieset.num
+        numerator_dict = {}
+        for i in range(self.minroll, self.maxroll + 1):
+            numerator_dict[i] = 0
+        for dieset in self.diesets:
+            if dieset.negative:
+                setroll = product(range(dieset.sides * -1, 0), repeat=dieset.num)
+            else:
+                setroll = product(range(1, dieset.sides + 1), repeat=dieset.num)
+            setrolls.append(setroll)
+            print(f"setroll: {setroll}")
+            for roll in setroll:
+                print(f"{roll}")
+        print(f"setrolls: {setrolls}")
+        allrolls = product(*setrolls)
+        print(f"allrolls: {allrolls}")
+        for roll in allrolls:
+            print(f"roll: {roll}")
+            numerator_dict[sum(roll)] += 1
+        for result in range(self.minroll, self.maxroll + 1):
+            results.append((result,
+                            round(numerator_dict[result] / denominator, 5)))
+        return results
 
 
 def dstring_tokenize(dice_string):

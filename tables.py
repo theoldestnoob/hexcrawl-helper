f46d952673b216d2be0029c187a6afc87ac192c1
theoldestnoob <will.hinz@gmail.com>
2021-03-24 22:35:59 -0500

    initial work on base Table class

--- tables.py
+++ tables.py
@@ -5,3 +5,57 @@ Created on Wed Aug 21 19:17:43 2019
 @author: theoldestnoob
 """
 
+from random import randint
+
+import dice
+
+
+class Table:
+    def __init__(self, name, diestr=None, entries=None):
+        self.name = name
+        self.set_dice(diestr)
+        if entries is not None:
+            self._entries = entries
+        else:
+            self._entries = []
+
+    def append(self, entry):
+        self._entries.append(entry)
+
+    def __str__(self):
+        return self.name
+
+    def __len__(self):
+        if self._dice is None:
+            return len(self._entries)
+        else:
+            return self._dice.rollrange
+
+    def __getitem__(self, position):
+        index = 1
+        if self._dice is not None:
+            if position < self._dice.minroll or position > self._dice.maxroll:
+                raise IndexError
+            index = self._dice.minroll
+        return self._entries[position - index]
+
+    def get_name(self):
+        return self.name
+
+    def get_entry(self, depth=0):
+        if depth > 10:
+            return 'Error: Maximum Recursion'
+        if self._dice is None:
+            index = randint(1, len(self._entries))
+        else:
+            index = self._dice.roll()
+        if hasattr(self.__getitem__(index), 'get_entry'):
+            return self.__getitem__(index).get_entry(depth + 1)
+        else:
+            return self.__getitem__(index)
+
+    def set_dice(self, diestr):
+        if diestr is None:
+            self._dice = None
+        else:
+            self._dice = dice.DieSet(diestr)
--- tests/test_tables.py
+++ tests/test_tables.py
@@ -5,3 +5,78 @@ Created on Wed Aug 21 19:28:40 2019
 @author: theoldestnoob
 """
 
+import unittest
+
+import tables
+
+
+class TestTable(unittest.TestCase):
+
+    def setUp(self):
+        self.entry_list = ['Entry 1-1', 'Entry 1-2', 'Entry 1-3', 'Entry 1-4',
+                           'Entry 1-5', 'Entry 1-6']
+        self.table = tables.Table('Table 1')
+        for e in self.entry_list:
+            self.table.append(e)
+
+    def test_Table_name(self):
+        self.assertEqual(self.table.name, 'Table 1')
+
+    def test_Table_insert_entry_string(self):
+        self.assertEqual(self.table._entries, self.entry_list)
+
+    def test_Table_len(self):
+        self.assertEqual(len(self.table), 6)
+        self.table.set_dice('2d3')
+        self.assertEqual(len(self.table), 5)
+
+    def test_Table_getitem_no_die(self):
+        self.assertEqual(self.table[1], 'Entry 1-1')
+        self.assertEqual(self.table[2], 'Entry 1-2')
+        self.assertEqual(self.table[3], 'Entry 1-3')
+        self.assertEqual(self.table[4], 'Entry 1-4')
+        self.assertEqual(self.table[5], 'Entry 1-5')
+        self.assertEqual(self.table[6], 'Entry 1-6')
+
+    def test_Table_getitem_with_die(self):
+        self.table.set_dice('2d3')
+        self.assertEqual(self.table[2], 'Entry 1-1')
+        self.assertEqual(self.table[3], 'Entry 1-2')
+        self.assertEqual(self.table[4], 'Entry 1-3')
+        self.assertEqual(self.table[5], 'Entry 1-4')
+        self.assertEqual(self.table[6], 'Entry 1-5')
+        with self.assertRaises(IndexError):
+            self.table[1]
+        with self.assertRaises(IndexError):
+            self.table[7]
+
+    def test_Table_get_entry_entries(self):
+        for _ in range(1000):
+            self.assertIn(self.table.get_entry(), self.entry_list)
+        self.table.set_dice('2d3')
+        for _ in range(1000):
+            self.assertIn(self.table.get_entry(), ['Entry 1-1', 'Entry 1-2',
+                                                   'Entry 1-3', 'Entry 1-4',
+                                                   'Entry 1-5'])
+
+    def test_Table_get_entry_with_table(self):
+        table2 = tables.Table('Table 2', diestr='1d4')
+        table2.append('Entry 2-1')
+        table2.append('Entry 2-2')
+        table2.append('Entry 2-3')
+        table2.append('Entry 2-4')
+        entry_list_2 = ['Entry 2-1', 'Entry 2-2', 'Entry 2-3', 'Entry 2-4']
+        entry_list_2.extend(self.entry_list)
+        self.table.append(table2)
+        for _ in range(1000):
+            self.assertIn(self.table.get_entry(), entry_list_2)
+
+    def test_Table_circular_ref_error(self):
+        table1 = tables.Table('Table1')
+        table2 = tables.Table('Table2')
+        table1.append(table2)
+        table2.append(table1)
+        self.assertEqual(table1.get_entry(), 'Error: Maximum Recursion')
+
+if __name__ == '__main__':
+    unittest.main()

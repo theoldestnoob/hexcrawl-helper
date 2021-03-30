783ae59f9d7e8180cc71d13c9b998c6050e630f4
theoldestnoob <will.hinz@gmail.com>
2021-03-29 23:54:21 -0500

    add TableEntry class, table vs entries list in Table class

--- tables.py
+++ tables.py
@@ -4,47 +4,92 @@ Created on Wed Aug 21 19:17:43 2019
 
 @author: theoldestnoob
 """
-
+from __future__ import annotations
 from random import randint
 
 import dice
 
 
-class Table:
-    def __init__(self, name, diestr=None, entries=None):
+class TableEntry:
+    def __init__(self, name: str, priority: int = 99, pos_wt: int = 0,
+                 prob_wt: int = 0, notes: str = None) -> None:
+        # brief description of table entry
         self.name = name
-        self.set_dice(diestr)
+        # priority of entry: lower = higher priority
+        self.priority = priority
+        # position weight
+        #   positive if entry wants to be high on table
+        #   negative if entry wants to be low on table
+        #   magnitude determines how much it wants to be high or low
+        self.pos_weight = pos_wt
+        # probability weight
+        #   positive if entry wants to be more probable
+        #   negative if entry wants to be less probable
+        self.prob_weight = prob_wt
+        # longer string for more detail on entry
+        self.notes = notes
+
+    def __eq__(self, other) -> bool:
+        return (self.name == other.name
+                and self.priority == other.priority
+                and self.pos_weight == other.pos_weight
+                and self.prob_weight == other.prob_weight
+                and self.notes == other.notes)
+
+    def get_entry(self, depth: int = 0) -> TableEntry:
+        return self
+
+
+class Table:
+    def __init__(self, name: str, diestr: dice.DieSet = None,
+                 entries: list[TableEntry] = None) -> None:
+        self.name = name
         if entries is not None:
             self._entries = entries
         else:
             self._entries = []
+        self._table = []
+        self.set_dice(diestr)
+        self.gen_table()
 
-    def append(self, entry):
+    def append(self, entry) -> None:
         self._entries.append(entry)
+        self._changed = True
 
-    def __str__(self):
+    def remove(self, entry) -> None:
+        self._entries.remove(entry)
+        self._changed = True
+
+    def clear(self, entry) -> None:
+        self._entries.clear()
+        self._changed = True
+
+    def __str__(self) -> str:
         return self.name
 
-    def __len__(self):
+    def __len__(self) -> int:
         if self._dice is None:
             return len(self._entries)
         else:
             return self._dice.rollrange
 
-    def __getitem__(self, position):
+    # TODO: rewrite __getitem__ and get_entry to accomodate large tables
+    #       with passed-in modifiers that exceed the dice
+    #       and/or custom dice passed to get the item
+    def __getitem__(self, position: int) -> TableEntry:
+        if self._changed:
+            self.gen_table()
         index = 1
         if self._dice is not None:
             if position < self._dice.minroll or position > self._dice.maxroll:
                 raise IndexError
             index = self._dice.minroll
-        return self._entries[position - index]
+        return self._table[position - index]
 
-    def get_name(self):
-        return self.name
-
-    def get_entry(self, depth=0):
+    def get_entry(self, depth: int = 0) -> TableEntry:
         if depth > 10:
-            return 'Error: Maximum Recursion'
+            error_entry = TableEntry('Error: Recursive Tables')
+            return error_entry
         if self._dice is None:
             index = randint(1, len(self._entries))
         else:
@@ -54,8 +99,27 @@ class Table:
         else:
             return self.__getitem__(index)
 
-    def set_dice(self, diestr):
+    def set_dice(self, diestr) -> None:
         if diestr is None:
             self._dice = None
         else:
             self._dice = dice.DieSet(diestr)
+        self._changed = True
+
+    # TODO: it seems silly to have a get_name function instead of just
+    #       directly accessing the attribute on the other hand, there may be
+    #       value as part of a function for *display* access vs *roll* access
+    def get_name(self) -> str:
+        return self.name
+
+    def gen_table(self) -> None:
+        if self._dice is None:
+            for e in self._entries:
+                self._table.append(e)
+        else:
+            tablesize = self._dice.rollrange
+            table = sorted(self._entries, key=lambda entry: entry.prob_weight,
+                           reverse=True)
+            table = sorted(self._entries, key=lambda entry: entry.priority)
+            self._table = table[:tablesize]
+        self._changed = False
--- tests/test_tables.py
+++ tests/test_tables.py
@@ -10,41 +10,58 @@ import unittest
 import tables
 
 
+class TestTableEntry(unittest.TestCase):
+
+    def test_TableEntry_eq(self):
+        entry = tables.TableEntry('Test')
+        entry2 = tables.TableEntry('Test')
+        self.assertIsNot(entry, entry2)
+        self.assertEqual(entry, entry2)
+
+    def test_TableEntry_get_entry(self):
+        entry = tables.TableEntry('Test')
+        entry2 = entry.get_entry()
+        self.assertIs(entry, entry2)
+
+
 class TestTable(unittest.TestCase):
 
     def setUp(self):
-        self.entry_list = ['Entry 1-1', 'Entry 1-2', 'Entry 1-3', 'Entry 1-4',
-                           'Entry 1-5', 'Entry 1-6']
+        e_list = ['Entry 1-1', 'Entry 1-2', 'Entry 1-3', 'Entry 1-4',
+                  'Entry 1-5', 'Entry 1-6']
+        self.entry_list = []
+        for entry in e_list:
+            self.entry_list.append(tables.TableEntry(entry))
         self.table = tables.Table('Table 1')
         for e in self.entry_list:
             self.table.append(e)
+        self.table.gen_table()
 
     def test_Table_name(self):
-        self.assertEqual(self.table.name, 'Table 1')
+        table = tables.Table('Table 1')
+        self.assertEqual(table.name, 'Table 1')
 
-    def test_Table_insert_entry_string(self):
-        self.assertEqual(self.table._entries, self.entry_list)
-
-    def test_Table_len(self):
-        self.assertEqual(len(self.table), 6)
-        self.table.set_dice('2d3')
-        self.assertEqual(len(self.table), 5)
+    def test_Table_append_entry(self):
+        table = tables.Table('Table 1')
+        entry = tables.TableEntry('Entry 1')
+        table.append(entry)
+        self.assertEqual(table._entries[0], entry)
 
     def test_Table_getitem_no_die(self):
-        self.assertEqual(self.table[1], 'Entry 1-1')
-        self.assertEqual(self.table[2], 'Entry 1-2')
-        self.assertEqual(self.table[3], 'Entry 1-3')
-        self.assertEqual(self.table[4], 'Entry 1-4')
-        self.assertEqual(self.table[5], 'Entry 1-5')
-        self.assertEqual(self.table[6], 'Entry 1-6')
+        self.assertEqual(self.table[1].name, 'Entry 1-1')
+        self.assertEqual(self.table[2].name, 'Entry 1-2')
+        self.assertEqual(self.table[3].name, 'Entry 1-3')
+        self.assertEqual(self.table[4].name, 'Entry 1-4')
+        self.assertEqual(self.table[5].name, 'Entry 1-5')
+        self.assertEqual(self.table[6].name, 'Entry 1-6')
 
     def test_Table_getitem_with_die(self):
         self.table.set_dice('2d3')
-        self.assertEqual(self.table[2], 'Entry 1-1')
-        self.assertEqual(self.table[3], 'Entry 1-2')
-        self.assertEqual(self.table[4], 'Entry 1-3')
-        self.assertEqual(self.table[5], 'Entry 1-4')
-        self.assertEqual(self.table[6], 'Entry 1-5')
+        self.assertEqual(self.table[2].name, 'Entry 1-1')
+        self.assertEqual(self.table[3].name, 'Entry 1-2')
+        self.assertEqual(self.table[4].name, 'Entry 1-3')
+        self.assertEqual(self.table[5].name, 'Entry 1-4')
+        self.assertEqual(self.table[6].name, 'Entry 1-5')
         with self.assertRaises(IndexError):
             self.table[1]
         with self.assertRaises(IndexError):
@@ -55,18 +72,17 @@ class TestTable(unittest.TestCase):
             self.assertIn(self.table.get_entry(), self.entry_list)
         self.table.set_dice('2d3')
         for _ in range(1000):
-            self.assertIn(self.table.get_entry(), ['Entry 1-1', 'Entry 1-2',
-                                                   'Entry 1-3', 'Entry 1-4',
-                                                   'Entry 1-5'])
+            self.assertIn(self.table.get_entry(), self.entry_list[:5])
 
     def test_Table_get_entry_with_table(self):
         table2 = tables.Table('Table 2', diestr='1d4')
-        table2.append('Entry 2-1')
-        table2.append('Entry 2-2')
-        table2.append('Entry 2-3')
-        table2.append('Entry 2-4')
-        entry_list_2 = ['Entry 2-1', 'Entry 2-2', 'Entry 2-3', 'Entry 2-4']
+        e_list_2 = ['Entry 2-1', 'Entry 2-2', 'Entry 2-3', 'Entry 2-4']
+        entry_list_2 = []
+        for e in e_list_2:
+            entry_list_2.append(tables.TableEntry(e))
         entry_list_2.extend(self.entry_list)
+        for entry in entry_list_2:
+            table2.append(entry)
         self.table.append(table2)
         for _ in range(1000):
             self.assertIn(self.table.get_entry(), entry_list_2)
@@ -76,7 +92,23 @@ class TestTable(unittest.TestCase):
         table2 = tables.Table('Table2')
         table1.append(table2)
         table2.append(table1)
-        self.assertEqual(table1.get_entry(), 'Error: Maximum Recursion')
+        error_entry = tables.TableEntry('Error: Recursive Tables')
+        self.assertEqual(table1.get_entry(), error_entry)
+
+    def test_Table_entry_priority(self):
+        table = tables.Table('Test')
+        table.append(tables.TableEntry('Prio0-1', priority=0))
+        table.append(tables.TableEntry('Prio0-2', priority=0))
+        table.append(tables.TableEntry('Prio5-1', priority=5))
+        table.append(tables.TableEntry('Prio5-2', priority=5))
+        table.append(tables.TableEntry('Prio0-3', priority=0))
+        table.append(tables.TableEntry('Prio0-4', priority=0))
+        table.set_dice('1d4')
+        table.gen_table()
+        for x in table._table:
+            self.assertEqual(x.priority, 0)
+
+
 
 if __name__ == '__main__':
     unittest.main()
